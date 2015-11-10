5ed8f4fc4cc3208bed07c34ef6d9cc6eb99dc55c
Rocco Meli <rocco.meli@epfl.ch>
2015-11-10 23:38:51 +0100

    One-electron and two-electron Gaussian integrals

---
+++ integrals.py
@@ -0,0 +1,60 @@
+import numpy as np
+import scipy.special as spec
+
+def gaussian_product(aa,bb,Ra,Rb):
+
+    a = aa + bb
+
+    R = (aa * Ra + bb * Rb) / (aa + bb)
+
+    c = np.dot(Ra-Rb,Ra-Rb)
+    c *= - aa*bb / (aa + bb)
+    c = np.exp(c)
+
+    return a,R,c
+
+def overlap_ss(aa,bb,Ra,Rb):
+    a,R,c = gaussian_product(aa,bb,Ra,Rb)
+
+    overlap = np.sqrt((np.pi / a)**3)
+    overlap *= c
+    overlap *= (2*aa/np.pi)**(3./4.) * (2*bb/np.pi)**(3./4.) # Normalization
+
+    return overlap
+
+def kinetic_ss(aa,bb,Ra,Rb):
+    o = overlap_ss(aa,bb,Ra,Rb)
+
+    k = aa * bb / (aa + bb)
+    k *= 3 - 2 * k * np.dot(Ra-Rb,Ra-Rb)
+    k *= o # Normalization is already contained here
+
+    return k
+
+def nuclear_ss(aa,bb,Ra,Rb,Zn,Rn):
+    a,R,c = gaussian_product(aa,bb,Ra,Rb)
+
+    n = -2 * np.pi / a * Zn * c
+    n *= F_ss(a * np.dot(R-Rn,R-Rn))
+    n *= (2*aa/np.pi)**(3./4.) * (2*bb/np.pi)**(3./4.) # Normalization
+
+    return n
+
+def electronic_ss(aa,bb,cc,dd,Ra,Rb,Rc,Rd):
+    p,Rp,cp = gaussian_product(aa,bb,Ra,Rb)
+    q,Rq,cq = gaussian_product(cc,dd,Rc,Rd)
+
+    e = 2 * np.pi**(5./2.) / (p * q * np.sqrt(p+q))
+    e *= cp * cq
+    e *= F_ss(p * q / (p + q) * np.dot(Rp-Rq,Rp-Rq))
+    e *= (2*aa/np.pi)**(3./4.) * (2*bb/np.pi)**(3./4.) # Normalization
+    e *= (2*cc/np.pi)**(3./4.) * (2*dd/np.pi)**(3./4.) # Normalization
+
+    return e
+
+def F_ss(t,cutoff=1e-6):
+    if abs(t) < cutoff:
+        return 1. - t / 3.
+    else:
+        # Eq. A.32
+        return 0.5 * np.sqrt(np.pi / t) * spec.erf(np.sqrt(t))
