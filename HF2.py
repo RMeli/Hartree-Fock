8f9a511c0fa87049b7c0b90dead3d86c03681d61
Rocco Meli <rocco.meli@epfl.ch>
2015-11-17 23:49:10 +0100

    Some molecules

--- HF2.py
+++ HF2.py
@@ -3,45 +3,10 @@ from RHF import *
 from matrices2 import *
 from integrals2 import *
 from basis2 import *
+from molecules import *
 
 import numpy as np
 import numpy.linalg as la
-
-# He
-He = [Atom("He",(0,0,0),2,["1s"])]
-sto3g_He = STO3G(He)
-
-# B
-Be = [Atom("Be",(0,0,0),4,["1s","2s"])]
-sto3g_Be = STO3G(Be)
-
-# N2
-N2 = [  Atom("N",(0,0,0),7,["1s","2s","2p"]),
-        Atom("N",(0,0,2.074),7,["1s","2s","2p"])]
-sto3g_N2 = STO3G(N2)
-
-# HF
-HF = [  Atom("H",(0,0,0),1,["1s"]),
-        Atom("F",(0,0,1.807),9,["1s","2s","2p"])]
-sto3g_HF = STO3G(HF)
-
-# B
-O = [Atom("O",(0,0,0),8,["1s","2s","2p"])]
-sto3g_O = STO3G(O)
-
-# H2
-H2 = [Atom("H",(0,0,0),1,["1s"]),Atom("H",(0,0,1.4),1,["1s"])]
-sto3g_H2 = STO3G(H2)
-
-# HeH+
-HeH = [Atom("He",(0,0,1.4632),2,["1s"]),Atom("H",(0,0,0),1,["1s"])]
-sto3g_HeH = STO3G(HeH)
-
-# H2O
-H2O = [ Atom("H",(1.638036840407,1.136548822547,0.000000000000),1,["1s"]),
-        Atom("H",(-1.638036840407,1.136548822547,0.000000000000),1,["1s"]),
-        Atom("O",(0.000000000000,-0.143225816552,0.000000000000),8,["1s","2s","2p"])]
-sto3g_H2O = STO3G(H2O)
 
 
 # Molecule
---
+++ molecules.py
@@ -0,0 +1,35 @@
+# He
+He = [Atom("He",(0,0,0),2,["1s"])]
+sto3g_He = STO3G(He)
+
+# B
+Be = [Atom("Be",(0,0,0),4,["1s","2s"])]
+sto3g_Be = STO3G(Be)
+
+# N2
+N2 = [  Atom("N",(0,0,0),7,["1s","2s","2p"]),
+        Atom("N",(0,0,2.074),7,["1s","2s","2p"])]
+sto3g_N2 = STO3G(N2)
+
+# HF
+HF = [  Atom("H",(0,0,0),1,["1s"]),
+        Atom("F",(0,0,1.807),9,["1s","2s","2p"])]
+sto3g_HF = STO3G(HF)
+
+# B
+O = [Atom("O",(0,0,0),8,["1s","2s","2p"])]
+sto3g_O = STO3G(O)
+
+# H2
+H2 = [Atom("H",(0,0,0),1,["1s"]),Atom("H",(0,0,1.4),1,["1s"])]
+sto3g_H2 = STO3G(H2)
+
+# HeH+
+HeH = [Atom("He",(0,0,1.4632),2,["1s"]),Atom("H",(0,0,0),1,["1s"])]
+sto3g_HeH = STO3G(HeH)
+
+# H2O
+H2O = [ Atom("H",(1.809*np.sin(104.52/2),0,0),1,["1s"]),
+        Atom("H",(-1.809*np.sin(104.52/2),0,0),1,["1s"]),
+        Atom("O",(0,1.809*np.cos(104.52/2),0),8,["1s","2s","2p"])]
+sto3g_H2O = STO3G(H2O)
