be4cf6d8cb2d83e5f51fe617d3ea8c40b15404f2
Rocco Meli <rocco.meli@epfl.ch>
2015-11-23 18:32:19 +0100

    Electron-electron integrals

--- f90/electronic.f90
+++ f90/electronic.f90
@@ -1,7 +1,9 @@
 MODULE ELECTRONIC
 
     USE FACT
-    USE NUCLEAR, only: f
+    USE GAUSSIAN
+    USE CONSTANTS
+    USE NUCLEAR, only: f, boys
 
     IMPLICIT NONE
 
@@ -81,13 +83,158 @@ MODULE ELECTRONIC
             !
             !--------------------------------------------------------------------------
 
-            ! INPUT
+            IMPLICIT NONE
 
+            ! INPUT
+            INTEGER, intent(in) :: ax, ay, az, bx, by, bz, cx, cy, cz, dx, dy, dz   ! Angular momentum coefficients
+            REAL*8, intent(in) :: aa, bb, cc, dd                                    ! Exponential Gaussian coefficients
+            REAL*8, dimension(3), intent(in) :: Ra, Rb, Rc, Rd                      ! Gaussian centers
 
             ! INTERMEDIATE VARIABLES
+            REAL*8 :: delta
+            REAL*8 :: g1, g2                        ! Gaussian produc exponential coefficient
+            REAL*8, dimension(3) :: Rp, Rq          ! Gaussian produc center
+            REAL*8 :: c1, c2                        ! Gaussian product multiplicative constant
+            REAL*8 :: BBx, BBy, BBz                 ! Temporary calls to FUNCTION B
+            INTEGER :: nu                           ! Boys function index
+            INTEGER :: l, r, i, ll, rr              ! Indices loop 1
+            INTEGER :: m, s, j, mm, ss              ! Indices loop 2
+            INTEGER :: n, t, k, nn, tt              ! Indices loop 3
 
             ! OUTPUT
+            REAL*8 :: G ! Electron-electron repulsion coefficient
+
+            delta = 1.0D0 / (4.0D0 * g1) + 1.0D0 / (4.0D0 * g2)
+
+            G = 0.0D0
+
+            CALL gaussian_product(aa,bb,Ra,Rb,g1,Rp,c1) ! Gaussian product of left gaussians
+            CALL gaussian_product(cc,dd,Rc,Rd,g2,Rq,c2) ! Gaussian product of right gaussians
+
+            DO l = 0, ax + bx
+                DO r = 0, FLOOR(l / 2.0)
+                    DO i = 0, FLOOR((l-2*r) / 2.0)
+                        DO ll = 0, cx + dx
+                            DO rr = 0, FLOOR(ll / 2.0)
+                                 BBx = B(l,ll,r,rr,i,ax,bx,Ra(1),Rb(1),Rp(1),g1,&
+                                         cx,dx,Rc(1),Rd(1),Rq(1),g2,delta)
+
+                                 DO m = 0, az + by
+                                     DO s = 0, FLOOR(m / 2.0)
+                                         DO j = 0, FLOOR((m-2*s) / 2.0)
+                                             DO mm = 0, cy + dy
+                                                 DO ss = 0, FLOOR(mm / 2.0)
+                                                    BBy = B(m,mm,s,ss,j,ay,by,Ra(2),Rb(2),Rp(2),&
+                                                            g1,cy,dy,Rc(2),Rd(2),Rq(2),g2,delta)
+
+                                                    DO n = 0, az + bz
+                                                        DO t = 0, FLOOR(n / 2.0)
+                                                            DO k = 0, FLOOR((n-2*t) / 2.0)
+                                                                DO nn = 0, cz + dz
+                                                                    DO tt = 0, FLOOR(nn / 2.0)
+
+                                                                        BBz = B(n,nn,t,tt,k,az,bz,Ra(3),Rb(3),Rp(3),g1,&
+                                                                                cz,dz,Rc(3),Rd(3),Rq(3),g2,delta)
+
+                                                                        nu = l+ll+m+mm+n+nn - 2*(r+rr+s+ss+t+tt) - (i + j + k)
+
+                                                                        G = G + BBx * BBy * BBz *&
+                                                                         boys(nu,DOT_PRODUCT(Rp-Rq,Rp-Rq)*g1*g2/(g1+g2))
+
+                                                                    END DO ! tt
+                                                                END DO ! nn
+                                                            END DO ! k
+                                                        END DO ! t
+                                                    END DO ! n
+
+                                                END DO ! ss
+                                            END DO ! mm
+                                        END DO ! j
+                                    END DO ! s
+                                END DO ! m
+
+                            END DO ! rr
+                        END DO ! ll
+                    END DO ! i
+                END DO ! r
+            END DO ! k
+
+        G = G * norm(ax,ay,az,aa) * norm(bx,by,bz,bb) * norm(cx,cy,cz,cc) * norm(dx,dy,dz,dd)
+        G = G * c1 * c2 * 2.0D0 * PI**2 / (g1 * g2) * SQRT(PI / (g1 + g2))
 
         END FUNCTION electronic_coeff
+
+        SUBROUTINE ee_list(Kf,basis_D,basis_A,basis_L,basis_R,ee)
+
+            IMPLICIT NONE
+
+            ! TODO Allow flexibility for basis sets other than STO-3G
+            ! HARD CODED
+            INTEGER, PARAMETER :: c = 3 ! Number of contractions per basis function
+
+            ! INPUT
+            INTEGER, intent(in) :: Kf                       ! Number of basis functions
+            REAL*8, dimension(Kf,3), intent(in) :: basis_R  ! Basis set niclear positions
+            INTEGER, dimension(Kf,3), intent(in) :: basis_L ! Basis set angular momenta
+            REAL*8, dimension(Kf,c), intent(in) :: basis_D  ! Basis set contraction coefficients
+            REAL*8, dimension(Kf,c), intent(in) :: basis_A  ! Basis set exponential contraction coefficients
+
+            ! INTERMEDIATE VARIABLES
+            INTEGER :: i,j,k,l  ! Indices running over basis functions
+            INTEGER :: m,n,o,p  ! Indices running over contractions
+            REAL*8 :: tmp
+
+            ! OUTPUT
+            REAL*8, dimension(Kf,Kf,Kf,Kf), intent(out) :: ee   ! List of electron-electron integrals
+            ee(:,:,:,:) = 0.0D0
+
+            DO i = 1,Kf
+                DO j = 1,Kf
+                    DO k = 1,Kf
+                        DO l = 1,Kf
+
+                            DO m = 1,c
+                                DO n = 1,c
+                                    DO o = 1,c
+                                        DO p = 1,c
+
+                                            ! Contraction coefficients
+                                            tmp = basis_D(i,m) * basis_D(j,n) * basis_D(k,o) * basis_D(l,p)
+
+                                            tmp = tmp * electronic_coeff(   basis_L(i,1),&  ! lx for basis function i
+                                                                            basis_L(i,2),&  ! ly for basis function i
+                                                                            basis_L(i,3),&  ! lz for basis function i
+                                                                            basis_L(j,1),&  ! lx for basis function j
+                                                                            basis_L(j,2),&  ! ly for basis function j
+                                                                            basis_L(j,3),&  ! lz for basis function j
+                                                                            basis_L(k,1),&  ! lx for basis function k
+                                                                            basis_L(k,2),&  ! ly for basis function k
+                                                                            basis_L(k,3),&  ! lz for basis function k
+                                                                            basis_L(l,1),&  ! lx for basis function l
+                                                                            basis_L(l,2),&  ! ly for basis function l
+                                                                            basis_L(l,3),&  ! lz for basis function l
+                                                                            basis_A(i,m),&  ! Exponential coefficient for basis function i, contraction m
+                                                                            basis_A(j,n),&  ! Exponential coefficient for basis function j, contraction n
+                                                                            basis_A(k,o),&  ! Exponential coefficient for basis function k, contraction o
+                                                                            basis_A(l,p),&  ! Exponential coefficient for basis function l, contraction p
+                                                                            basis_R(i,:),&  ! Center of basis function i
+                                                                            basis_R(j,:),&  ! Center of basis function j
+                                                                            basis_R(k,:),&  ! Center of basis function i
+                                                                            basis_R(l,:))   ! Center of basis function j
+
+
+                                            ee(i,j,k,l) = ee(i,j,k,l) + tmp
+
+                                        END DO ! p
+                                    END DO ! o
+                                END DO ! m
+                            END DO ! n
+
+                        END DO ! l
+                    END DO ! k
+                END DO ! j
+            END DO ! i
+
+        END SUBROUTINE ee_list
 
 END MODULE ELECTRONIC
--- f90/utils.f90
+++ f90/utils.f90
@@ -9,6 +9,8 @@ MODULE UTILS
     ! ------------
     SUBROUTINE print_real_matrix(r,c,M)
 
+        IMPLICIT NONE
+
         ! INPUT
         INTEGER, intent(in) :: r, c
         REAL*8, dimension(r,c), intent(in) :: M
@@ -21,5 +23,33 @@ MODULE UTILS
         END DO
 
     END SUBROUTINE print_real_matrix
+
+    ! --------------------------------------
+    ! PRINT ELECTRON-ELECTRON INTEGRALS LIST
+    ! --------------------------------------
+    SUBROUTINE print_ee_list(Kf,ee)
+
+        IMPLICIT NONE
+
+        ! INPUT
+        INTEGER, intent(in) :: Kf               ! Basis set size
+        REAL*8, dimension(Kf,Kf,Kf,Kf) :: ee    ! List of electron-electron integrals
+
+        ! INTERMEDIATE VARIABLES
+        INTEGER :: i, j, k, l   ! Loop indices
+
+        DO i = 1,Kf
+            DO j = 1,Kf
+                DO k = 1,Kf
+                    DO l = 1,Kf
+
+                        WRITE(*,*) "(", i, j, k, l, ") ", ee(i,j,k,l)
+
+                    END DO ! l
+                END DO ! k
+            END DO ! j
+        END DO ! i
+
+    END SUBROUTINE print_ee_list
 
 END MODULE UTILS
