5a3af05b973c271feef1eaeb5ad954ee17c2bf6f
Rocco Meli <rocco.meli@epfl.ch>
2015-12-04 18:03:26 +0100

    GPL added

--- Fortran90/tests/factorial_test.f90
+++ Fortran90/tests/factorial_test.f90
@@ -1,7 +1,23 @@
-!-----
-! TEST
-!-----
-PROGRAM test
+! --------------------------------------------------------------------
+!
+! Copyright (C) 2015 Rocco Meli
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!
+! ---------------------------------------------------------------------
+
+PROGRAM factorial_test
 
     USE FACT
 
@@ -21,4 +37,4 @@ PROGRAM test
     DO i = 0,N
         WRITE(*,*) "binom(10,", i, ") =", binom(10,i)
     END DO
-END PROGRAM test
+END PROGRAM factorial_test
