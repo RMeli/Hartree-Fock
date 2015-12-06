08e3333e3b8f52b87f5fffb25f72465d04d62616
Rocco Meli <rocco.meli@epfl.ch>
2015-12-06 21:45:07 +0100

    Electronic and total energy for spin orbitals

--- Fortran90/energy.f90
+++ Fortran90/energy.f90
@@ -24,6 +24,14 @@ MODULE ENERGY
     CONTAINS
 
         FUNCTION E_electronic(Kf,P,F,H) result(E_el)
+            ! -
+            !
+            ! -
+            !
+            ! Source:
+            !   A. Szabo and N. Ostlund
+            !
+            ! -
 
             IMPLICIT NONE
 
@@ -43,11 +51,44 @@ MODULE ENERGY
 
             DO i = 1, Kf
                 DO j = 1, Kf
-                    E_el = E_el + 0.5D0 * P(i,j) * (H(i,j) + F(i,j))
+                    E_el = E_el + 0.5D0 * P(j,i) * (H(i,j) + F(i,j))
                 END DO
             END DO
 
         END FUNCTION E_electronic
+
+        FUNCTION E_electronic_spin(Kf,Pa,Fa,Pb,Fb,H) result(E_el)
+
+            IMPLICIT NONE
+
+            ! INPUT
+            INTEGER, intent(in) :: Kf
+            REAL*8, dimension(Kf,Kf), intent(in) :: Pa      ! Density matrix (alpha spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: Pb      ! Density matrix (beta spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: Fa      ! Fock operator (alpha spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: Fb      ! Fock operator (beta spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: H       ! Core Hamiltonian
+
+            ! INTERMEDIATE VARIABLES
+            INTEGER :: i, j                                 ! Loop indices
+            REAL*8, dimension(Kf,Kf) :: Ptot                ! Total density matrix
+
+            ! OUTPUT
+            REAL*8 :: E_el                              ! Electronic energy
+
+            Ptot = Pa + Pb
+
+            E_el = 0.0D0
+
+            DO i = 1, Kf
+                DO j = 1, Kf
+                    E_el = E_el + Ptot(j,i) * H(i,j) + Pa(j,i) * Fa(i,j) + Pb(j,i) * Fb(i,j)
+                END DO
+            END DO
+
+            E_el = 0.5D0 * E_el
+
+        END FUNCTION E_electronic_spin
 
 
         FUNCTION E_nuclear(Nn,Rn,Zn) result(E_nu)
@@ -93,6 +134,27 @@ MODULE ENERGY
             E_tot = E_nuclear(Nn,Rn,Zn) + E_electronic(Kf,P,F,H)
 
         END FUNCTION E_tot
+
+        FUNCTION E_tot_spin(Kf,Nn,Rn,Zn,Pa,Pb,Fa,Fb,H) result(E_tot)
+            IMPLICIT NONE
+
+            ! INPUT
+            INTEGER, intent(in) :: Kf                   ! Basis set size
+            INTEGER, intent(in) :: Nn                   ! Total number of nuclei
+            INTEGER, dimension(Nn), intent(in) :: Zn    ! Nuclear charges
+            REAL*8, dimension(Nn,3), intent(in) :: Rn   ! Nuclear positions
+            REAL*8, dimension(Kf,Kf), intent(in) :: Pa   ! Density matrix (alpha spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: Fa   ! Fock operator (alpha spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: Pb   ! Density matrix (beta spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: Fb   ! Fock operator (beta spin)
+            REAL*8, dimension(Kf,Kf), intent(in) :: H   ! Core Hamiltonian
+
+            ! OUTPUT
+            REAL*8 :: E_tot
+
+            E_tot = E_nuclear(Nn,Rn,Zn) + E_electronic_spin(Kf,Pa,Fa,Pb,Fb,H)
+
+        END FUNCTION E_tot_spin
 
 
 
